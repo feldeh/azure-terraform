2a4a1254e22a3f8fb9b831077cc6a79fc703e500
felicien <felicien@feliciens-MacBook-Pro.local>
2024-03-06 22:54:28 +0100

    add subnet

--- main.tf
+++ main.tf
@@ -14,8 +14,15 @@ resource "azurerm_virtual_network" "vn" {
   name                = "vn"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  address_space       = ["10.123.0.0/16"]
+  address_space       = [ "10.123.0.0/16" ]
   tags = {
     environment = "dev"
   }
+}
+
+resource "azurerm_subnet" "subnet" {
+  name = "subnet"
+  resource_group_name = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vn.name
+  address_prefixes = [ "10.123.1.0/24" ]
 }
