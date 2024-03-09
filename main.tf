bdc21792d7b1176b000d9bed0c1e2024f83bf5cb
felicien <felicien@feliciens-MacBook-Pro.local>
2024-03-09 23:29:58 +0100

    add nic and vm

--- main.tf
+++ main.tf
@@ -41,9 +41,9 @@ resource "azurerm_network_security_rule" "dev-rule" {
   priority                    = 100
   direction                   = "Inbound"
   access                      = "Allow"
-  protocol                    = "*"
+  protocol                    = "Tcp"
   source_port_range           = "*"
-  destination_port_range      = "*"
+  destination_port_range      = "22"
   source_address_prefix       = "*"
   destination_address_prefix  = "*"
   resource_group_name         = azurerm_resource_group.rg.name
@@ -65,6 +65,46 @@ resource "azurerm_public_ip" "ip" {
   location            = azurerm_resource_group.rg.location
   allocation_method   = "Dynamic"
   tags = {
-    environment = "prod"
+    environment = "dev"
+  }
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "nic"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.ip.id
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                  = "vm"
+  resource_group_name   = azurerm_resource_group.rg.name
+  location              = azurerm_resource_group.rg.location
+  size                  = "Standard_B2ts_v2"
+  admin_username        = "adminuser"
+  network_interface_ids = [azurerm_network_interface.nic.id]
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("~/.ssh/azure_id.pub")
+  }
+  os_disk {
+    name                 = "os_disk"
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+  tags = {
+    environment = "dev"
   }
 }
