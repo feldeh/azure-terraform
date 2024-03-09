246a8bfabc54659138733077ed63c96a08b37ba2
felicien <felicien@feliciens-MacBook-Pro.local>
2024-03-09 22:24:02 +0100

    add sha and public ip

--- main.tf
+++ main.tf
@@ -14,15 +14,57 @@ resource "azurerm_virtual_network" "vn" {
   name                = "vn"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  address_space       = [ "10.123.0.0/16" ]
+  address_space       = ["10.123.0.0/16"]
   tags = {
     environment = "dev"
   }
 }
 
 resource "azurerm_subnet" "subnet" {
-  name = "subnet"
-  resource_group_name = azurerm_resource_group.rg.name
+  name                 = "subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vn.name
-  address_prefixes = [ "10.123.1.0/24" ]
+  address_prefixes     = ["10.123.1.0/24"]
+}
+
+resource "azurerm_network_security_group" "sg" {
+  name                = "sg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  tags = {
+    environment = "dev"
+  }
+}
+
+resource "azurerm_network_security_rule" "dev-rule" {
+  name                        = "dev-rule"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "*"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.sg.name
+}
+
+resource "azurerm_subnet_network_security_group_association" "sga" {
+  subnet_id                 = azurerm_subnet.subnet.id
+  network_security_group_id = azurerm_network_security_group.sg.id
+}
+
+resource "random_id" "ip_id" {
+  byte_length = 1
+}
+
+resource "azurerm_public_ip" "ip" {
+  name                = "ip-${random_id.ip_id.dec}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  allocation_method   = "Dynamic"
+  tags = {
+    environment = "prod"
+  }
 }
